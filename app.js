74cc83d6f2494f2db5f6e2bc1088a48c23763564
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-12 08:38:07 -0700

    Move product specs out to partial
    Add attribute directive to include partial

--- app.js
+++ app.js
@@ -1,47 +1,63 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
+
+  app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('TabController', function(){
+  app.controller("TabController", function() {
     this.tab = 1;
 
-    this.setTab = function(newValue){
-      this.tab = newValue;
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
     };
 
-    this.isSet = function(tabName){
-      return this.tab === tabName;
-    };
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
+    this.setTab = function(setTab) {
+      this.tab = setTab;
     };
   });
 
-  app.controller('ReviewController', function(){
+  app.controller("ReviewController", function(){
+
     this.review = {};
 
-    this.addReview = function(product) {
-      this.review.createdOn = Date.now();
+    this.addReview = function(product){
       product.reviews.push(this.review);
       this.review = {};
     };
+
   });
 
-  app.directive("productDescription", function() {
+  app.directive("productDescriptions", function() {
     return {
       restrict: 'E',
-      templateUrl: 'product-description.html'
+      templateUrl: "product-descriptions.html"
     };
   });
 
-  var gems = [{
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function() {
+    return {
+      restrict: "A",
+      templateUrl: "product-specs.html"
+    };
+  });
+
+  var gems = [
+    {
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
       shine: 8,
@@ -57,13 +73,11 @@
       reviews: [{
         stars: 5,
         body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
+        author: "joe@example.org"
       }, {
         stars: 1,
         body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
+        author: "tim@example.org"
       }]
     }, {
       name: 'Bloodstone',
@@ -76,47 +90,43 @@
       images: [
         "images/gem-01.gif",
         "images/gem-03.gif",
-        "images/gem-04.gif",
+        "images/gem-04.gif"
       ],
       reviews: [{
         stars: 3,
         body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
+        author: "JimmyDean@example.org"
       }, {
         stars: 4,
         body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
+        author: "gemsRock@example.org"
       }]
-    }, {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "images/gem-06.gif",
-        "images/gem-07.gif",
-        "images/gem-08.gif"
-      ],
-      reviews: [{
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
       }, {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }]
-    }];
-})();
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-08.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
+    }
+  ];
+})()
